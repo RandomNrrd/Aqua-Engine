d4cf76e959912b6e7e187d5fdf14fca9f23ed9b5
Jackson <59755586+RandomNrrd@users.noreply.github.com>
2021-01-14 15:20:22 -0600

    Delete Engine.py

--- Engine.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-print("Boot Successful")
-window = Tk()
-window.title("Aqua Engine")
-window.configure(width=640, height=480)
-window.configure(bg='lightgray')
-window.mainloop()
